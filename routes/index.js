5f0addc9e36fef8ecc30c4be156621d1c06a4f1d
gineikacc <gineikacc@gmail.com>
2024-12-04 23:47:14 +0200

    meems

--- routes/index.js
+++ routes/index.js
@@ -86,8 +86,8 @@ router.get("/getproductsofcheck", async function (req, res) {
     .then((arr) => {
       products = arr.map(p=>(
         {
-          check_name: p.check_name,
-          display_name: p.display_name,
+          checkName: p.check_name,
+          displayName: p.display_name,
           price: p.price,
           weight: p.weight,
           calories: p.calories,
