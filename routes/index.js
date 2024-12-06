8bfea5b41fafc7b16150c8ec96ccd455e0428118
gineikacc <gineikacc@gmail.com>
2024-12-06 14:23:45 +0200

    minor memes 762

--- routes/index.js
+++ routes/index.js
@@ -77,7 +77,7 @@ router.get("/receiptpurchases", async function (req, res) {
     .then((arr) => {
       purchases = arr.map(p=>(
         {
-          receiptID: p.receipt_id,
+          checkID: p.receipt_id,
           productID: p.product_id,
           cost: p.cost,
           amount: p.amount,
