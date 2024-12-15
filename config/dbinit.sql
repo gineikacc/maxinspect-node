1e35cd435f95518bfd27de49e02f58b49b453eba
gineika.cc <gineikacc@gmail.com>
2024-12-15 13:58:07 +0000

    dbinit sql fixed

--- config/dbinit.sql
+++ config/dbinit.sql
@@ -29,29 +29,3 @@ CREATE TABLE IF NOT EXISTS Purchases (
     PRIMARY KEY (receipt_id, product_id)
 );
 
-
-INSERT INTO Products (check_name, display_name, price, weight, calories, protein, carbs, fats) 
-VALUES ('apple_1kg', 'Apple (1kg)', 300, 1000, 52, 0.3, 14, 0.2);
-
-INSERT INTO Products (check_name, display_name, price, weight, calories, protein, carbs, fats) 
-VALUES ('chicken_breast', 'Chicken Breast', 800, 0, 165, 31, 0, 3.6);
-
-INSERT INTO Products (check_name, display_name, price, weight, calories, protein, carbs, fats) 
-VALUES ('rice_1kg', 'White Rice (1kg)', 200, 1000, 365, 7.1, 80, 0.6);
-
-INSERT INTO Products (check_name, display_name, price, weight, calories, protein, carbs, fats) 
-VALUES ('almonds_500g', 'Almonds (500g)', 1500, 500, 579, 21, 22, 50);
-
-INSERT INTO Products (check_name, display_name, price, weight, calories, protein, carbs, fats) 
-VALUES ('carrot_loose', 'Carrot (Loose)', 50, 0, 41, 0.9, 10, 0.2);
-
-INSERT INTO Products (check_name, display_name, price, weight, calories, protein, carbs, fats) 
-VALUES ('milk_1l', 'Whole Milk (1L)', 120, 1000, 61, 3.3, 4.8, 3.3);
-
-INSERT INTO Products (check_name, display_name, price, weight, calories, protein, carbs, fats) 
-VALUES ('olive_oil_1l', 'Olive Oil (1L)', 2500, 1000, 884, 0, 0, 100);
-
-INSERT INTO Products (check_name, display_name, price, weight, calories, protein, carbs, fats) 
-VALUES ('potato_2kg', 'Potato (2kg)', 300, 2000, 77, 2, 17, 0.1);
-
-INSERT INTO `Receipts`(`id`,`owner_name`,`date_issued`,`cost_total`) VALUES(12345678,'airolen11@gmail.com','2024-11-13 20:06:01',1234);
